1071deae830c6374287f932a5d14afe1b5e2357e
Maia Dutta <mermaia33@gmail.com>
2023-02-02 17:06:31 -0600

    fixtures and seed data added

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 
 class Game(models.Model):
@@ -7,6 +7,6 @@ class Game(models.Model):
     game_title = models.CharField(max_length=100)
     type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name='game_type')
     creator = models.ForeignKey("Gamer", null=True, blank=True, on_delete=models.CASCADE, related_name='game_creator')
-    skill_level = models.IntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])
+    skill_level = models.IntegerField()
     maker = models.CharField(max_length=50)
-    num_of_players = models.IntegerField(validators=MinValueValidator(1))
+    num_of_players = models.IntegerField()
