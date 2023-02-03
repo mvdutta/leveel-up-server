37edad19ba397f9a3787c657f086116511459e17
Maia Dutta <mermaia33@gmail.com>
2023-02-02 18:05:32 -0600

    list and retrieve methods for game_type working

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,7 +1,11 @@
+from rest_framework import routers
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from levelupapi.views import register_user, login_user, GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
@@ -9,4 +13,6 @@ urlpatterns = [
     # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+
 ]
