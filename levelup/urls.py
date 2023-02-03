5fc1c297dbd301332de059a0351fa75eb85f9146
Maia Dutta <mermaia33@gmail.com>
2023-02-02 18:25:44 -0600

    list and retrieve methods for events and games working

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,10 +2,12 @@ from rest_framework import routers
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user, GameTypeView
+from levelupapi.views import register_user, login_user, GameTypeView, EventView, GameView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
